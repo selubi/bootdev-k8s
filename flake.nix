09560ff786510741a2cac81f5090ac0cf281210f
selubi <gregorius.bryan@selubi.tech>
2026-08-21 23:14:44 +0900

    devenv

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "bootdev k8s course";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          kubectl
+          minikube
+        ];
+
+        shellHook = ''
+          export KUBECONFIG="$PWD/.kubeconfig"
+        '';
+      };
+    };
+}
